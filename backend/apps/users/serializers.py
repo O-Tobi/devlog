5c48ac4c8b624bfa0cf7b0fc20af8065b90877a1
unknown <dunjoyebiz2020@gmail.com>
2026-07-28 09:37:43 +0100

    feat: add UserSerializer and Register Serializer

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import User
+
+# API for User Model
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+        read_only_fields = ('id',)
+
+
+
+#API endpoint for user registration
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password2")
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Passwords do not match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+
+        return User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
