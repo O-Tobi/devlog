0a9e64d6f48fa0e9f8acb942b538fb851f4dad77
unknown <dunjoyebiz2020@gmail.com>
2026-07-31 10:44:49 +0100

    feat: wired up the login view

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .models import User
 from .serializers import RegisterSerializer
 
@@ -6,3 +7,4 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
--- backend/devlog/settings.py
+++ backend/devlog/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'apps.skills',
     'apps.logs',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 AUTH_USER_MODEL = 'users.User'
 
